e15c98b0e40050847f514aeb4461c0bce8086f4c
mrudhukar <mrudhukar@chronus.com>
2013-04-06 20:06:12 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lonoti::Application.config.secret_token = '088a395ab63e052e696509e1f5d7d5e97d963ca1b54b80d435bd32335cf4c3624493a998517631c5f38e4275263448779dec2c515219fa16c875ca45ad761da3'
